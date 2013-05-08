8fb08dbbfcde57c41e559671993b9497c0415125
jmhofer <jmhofer.github@johoop.de>
2013-05-08 18:09:43 +0200

    direction computation, initial graphics experiments

--- src/main/scala/de/johoop/rxjava/samples/swing/Predef.scala
+++ src/main/scala/de/johoop/rxjava/samples/swing/Predef.scala
@@ -4,11 +4,11 @@ import rx.util.functions.Func1
 import rx.util.functions.Func2
 
 object Predef {
-  implicit def func1[T, R](f: T => R): Func1[T, R] = new Func1[T, R] {
+  def func1[T, R](f: T => R): Func1[T, R] = new Func1[T, R] {
     override def call(t: T): R = f(t)
   }
 
-  implicit def func2[S, T, R](f: (S, T) => R): Func2[S, T, R] = new Func2[S, T, R] {
+  def func2[S, T, R](f: (S, T) => R): Func2[S, T, R] = new Func2[S, T, R] {
     override def call(s: S, t: T): R = f(s, t)
   }
 }
--- src/main/scala/de/johoop/rxjava/samples/swing/pong/Main.scala
+++ src/main/scala/de/johoop/rxjava/samples/swing/pong/Main.scala
@@ -14,6 +14,16 @@ import java.util.{ Set => JSet }
 import scala.collection.JavaConverters._
 import java.util.concurrent.TimeUnit
 import rx.concurrency.SwingScheduler
+import java.awt.Graphics2D
+import java.awt.event.KeyEvent
+import scala.collection.breakOut
+import rx.util.functions.Func1
+
+case class Inputs(width: Double, height: Double, direction: Option[Direction])
+
+sealed trait Direction
+case object Up extends Direction
+case object Down extends Direction
 
 object Main extends SimpleSwingApplication {
   lazy val top = new MainFrame {
@@ -26,15 +36,35 @@ object Main extends SimpleSwingApplication {
     background = Color.white
     focusable = true
   }
-  
+
   val size = SwingObservable fromResizing canvas.peer
-  val keys = SwingObservable fromPressedKeys canvas.peer
   
-  val rawInputs = Observable create combineLatest(size, keys, func2 { (size: Dimension, keys: JSet[Integer]) =>
-    ((size.getWidth, size.getHeight), keys.asScala)
+  val direction = SwingObservable fromPressedKeys canvas.peer map keySetToDirection
+  
+  val keySetToDirection = func1 { (keys: JSet[Integer]) =>
+    import KeyEvent._
+    
+    val result: Set[Direction] = (for {
+      key <- keys.asScala if key == VK_UP || key == VK_DOWN
+      direction = if (key == VK_UP) Up else Down
+    } yield direction)(breakOut)
+    
+    if (result.size == 1) Some(result.head) else None
+  }
+  
+  val inputs = Observable create combineLatest(size, direction, func2 { (size: Dimension, dir: Option[Direction]) =>
+    Inputs(size.getWidth, size.getHeight, dir)
   })
 
-  val sampled = rawInputs.sample(1L, TimeUnit.SECONDS, SwingScheduler.getInstance)
-  
-  val subSampled = sampled subscribe func1(println)
+  val sampled = inputs.sample(1L, TimeUnit.SECONDS, SwingScheduler.getInstance)
+
+  val subSampled = sampled subscribe func1({ inputs: Inputs =>
+    val graphics = canvas.peer.getGraphics.asInstanceOf[Graphics2D]
+    
+    graphics.clearRect(0, 0, inputs.width.toInt, inputs.height.toInt)
+    graphics setColor Color.black
+    val str = inputs.direction map { dir => if (dir == Up) "up" else "down" } getOrElse "none"
+    graphics.drawString(str, 100, 100)
+    // TODO canvas.paint(graphics)
+  })
 }
