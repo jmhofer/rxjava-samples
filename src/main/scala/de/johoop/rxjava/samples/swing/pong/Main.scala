c4deee794e7db22354190fe51ebe3aad24b2cffc
jmhofer <jmhofer.github@johoop.de>
2013-05-09 21:18:41 +0200

    Finally a working mouse controller

--- src/main/scala/de/johoop/rxjava/samples/swing/pong/Main.scala
+++ src/main/scala/de/johoop/rxjava/samples/swing/pong/Main.scala
@@ -21,15 +21,17 @@ import rx.util.functions.Func1
 import java.awt.event.MouseEvent
 import rx.operators.OperationCombineLatest
 import java.awt.Point
+import rx.util.Timestamped
 
-case class Inputs(player1: Option[Direction], player2: Option[Direction])
+case class Inputs(player1: Timestamped[Option[Direction]], player2: Option[Direction])
+case class State(player1: Option[Direction], player2: Option[Direction])
 
 sealed trait Direction
 case object Up extends Direction
 case object Down extends Direction
 
 object Main extends SimpleSwingApplication {
-  private var state: Inputs = Inputs(None, None) // TODO replace by observable
+  private var state: State = State(None, None) // TODO add real Pong stuff to state
   
   override def top = new MainFrame {
     title = "Rx Pong"
@@ -73,13 +75,19 @@ object Main extends SimpleSwingApplication {
   val player1Direction = mouse
       .map (func1[Point, Int]((_: Point).getY.toInt))
       .map (func1[Int, Option[Direction]]((dy: Int) => if (dy < 0) Some(Up) else if (dy > 0) Some(Down) else None))
+      .timestamp
   
-  val inputs = Observable create OperationCombineLatest.combineLatest(player1Direction, player2Direction, func2 { Inputs(_: Option[Direction], _: Option[Direction]) })
+  val inputs = Observable create OperationCombineLatest.combineLatest(player1Direction, player2Direction, func2 { Inputs((_: Timestamped[Option[Direction]]), (_: Option[Direction]))})
 
-  val sampled = inputs sample (40L, TimeUnit.MILLISECONDS, SwingScheduler.getInstance)
+  val sampled = inputs 
+      .sample (40L, TimeUnit.MILLISECONDS, SwingScheduler.getInstance)
+      .map (func1[Inputs, State] { (inputs: Inputs) =>
+        State(player1 = if (inputs.player1.getTimestampMillis < System.currentTimeMillis - 40L) None else inputs.player1.getValue,
+              player2 = inputs.player2)
+      })
 
-  sampled subscribe func1({ inputs: Inputs =>
-    state = inputs
+  sampled subscribe func1({ newState: State =>
+    state = newState
     canvas.repaint
   })
 }
