fd34c71ef3869ccfca87d4d83a55d4f1cd6db21d
jmhofer <jmhofer.github@johoop.de>
2013-08-29 21:54:30 +0200

    updated sbt version, updated to latest RxJava snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -8,19 +8,19 @@ object Build extends Build {
       settings = Project.defaultSettings ++ rootSettings)
 
   lazy val rootSettings: Seq[Setting[_]] = Seq(
-
     name := "rxjava-samples",
     version := "1.0.0-SNAPSHOT",
     organization := "de.johoop",
+    
     scalaVersion := "2.10.2",
-    scalacOptions += "-language:_",
+    scalacOptions ++= Seq("-deprecation", "-language:_"),
 
     resolvers += "Local Maven Repository" at Path.userHome.toURI + ".m2/repository",
 
     libraryDependencies ++= Seq(
       "org.scala-lang" % "scala-swing" % "2.10.2",
-      "com.netflix.rxjava" % "rxjava" % "0.10.1-SNAPSHOT" changing, // jmhofer/RxJava, branch multi-static-final, published locally
-      "com.netflix.rxjava" % "rxjava-scala" % "0.10.1-SNAPSHOT" changing() intransitive(),
-      "com.netflix.rxjava" % "rxjava-swing" % "0.10.1-SNAPSHOT" changing() intransitive(),
+      "com.netflix.rxjava" % "rxjava-core" % "0.11.0-SNAPSHOT" changing, // mattrjacobs/RxJava, branch static-core, published locally
+      "com.netflix.rxjava" % "rxjava-scala" % "0.11.0-SNAPSHOT" changing() intransitive(),
+      "com.netflix.rxjava" % "rxjava-swing" % "0.11.0-SNAPSHOT" changing() intransitive(),
       "org.slf4j" % "slf4j-simple" % "1.7.4"))
 }
